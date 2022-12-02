4bf1ed4d0520e408e2af8418b6e7f25bb3197e71
NewGodBR <newgodbr.mine@gmail.com>
2022-12-02 15:18:30 -0300

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+//importar scanner
+
+import java.util.Scanner;
+
+// Começo da classe:
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        //Definir variáveis e criar escaneador
+
+        String escolha;
+        
+        Scanner imput = new Scanner(System.in);
+
+        // Apresentação da historia
+
+        System.out.println("Um maluco está correndo e gritando ");
+
+        // Acontecimento anterior a decisão
+
+        System.out.println("vindo em sua direção:\n"+
+        
+        //Opções:
+        "[Correr]"+
+        "[Gritar]"+
+        "[Nada]");
+
+        // Tomada de Decisão
+
+        escolha = imput.nextLine();
+        
+        // Acontecimentos pós Decisão e por assim vai
+        
+        imput.close();
+    }
+}
